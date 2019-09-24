31ee0652965e2e949d38ca3a6881d25473e950c4
trijste <trijste@pm.me>
2019-09-23 22:46:59 -0400

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -54,45 +54,43 @@ $.featherlight.prototype.afterContent = function() {
 };
 //hash code
 function getHashFilter() {
-  var hash = location.hash;
   // get filter=filterName
   var matches = location.hash.match( /filter=([^&]+)/i );
   var hashFilter = matches && matches[1];
   return hashFilter && decodeURIComponent( hashFilter );
 }
 
-$( function() {
-
-  var $grid = $('.isotope');
-
-  // bind filter button click
-  var $filters = $('#filters').on( 'click', 'button', function() {
-    var filterAttr = $( this ).attr('data-filter');
-    // set filter in hash
-    location.hash = 'filter=' + encodeURIComponent( filterAttr );
-  });
-
-  var isIsotopeInit = false;
-
-  function onHashchange() {
-    var hashFilter = getHashFilter();
-    if ( !hashFilter && isIsotopeInit ) {
-      return;
-    }
-    isIsotopeInit = true;
-    // filter isotope
-    $grid.isotope({
-      itemSelector: '.content',
-      filter: hashFilter
-    });
-    // set selected class on button
-    if ( hashFilter ) {
-      $filters.find('.is-checked').removeClass('is-checked');
-      $filters.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
-    }
-  }
-
-  $(window).on( 'hashchange', onHashchange );
-  // trigger event handler to init Isotope
-  onHashchange();
+// bind filter button click
+var $filterButtonGroup = $('.button-group');
+$filterButtonGroup.on( 'click', 'button', function() {
+  var filterAttr = $( this ).attr('data-filter');
+  // set filter in hash
+  location.hash = 'filter=' + encodeURIComponent( filterAttr );
 });
+
+var isIsotopeInit = false;
+
+function onHashchange() {
+  var hashFilter = getHashFilter();
+  if ( !hashFilter && isIsotopeInit ) {
+    return;
+  }
+  isIsotopeInit = true;
+  // filter isotope
+  $grid.isotope({
+    itemSelector: '.content',
+    layoutMode: 'fitRows',
+    // use filterFns
+    filter: filterFns[ hashFilter ] || hashFilter
+  });
+  // set selected class on button
+  if ( hashFilter ) {
+    $filterButtonGroup.find('.is-checked').removeClass('is-checked');
+    $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
+  }
+}
+
+$(window).on( 'hashchange', onHashchange );
+
+// trigger event handler to init Isotope
+onHashchange();
